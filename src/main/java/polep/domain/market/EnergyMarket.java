18bfe23aef319d470ecefc7eee094ab16c1a5cfd
PCBhagwat <P.C.Bhagwat@tudelft.nl>
2013-01-31 15:19:05 +0100

    Update src/main/java/polep/domain/market/EnergyMarket.java

--- src/main/java/polep/domain/market/EnergyMarket.java
+++ src/main/java/polep/domain/market/EnergyMarket.java
@@ -4,7 +4,6 @@ package polep.domain.market;
  * Energy Market Agent establishes the value of lost load, which is a price cap in case the market doesn't clear. 
  * It also determines the clearing price, based on a sorting of all the bids received and an existing demand. 
  * 
- *question: is demand set here or is it part of the environment?
  */
 
 import org.springframework.data.neo4j.annotation.NodeEntity;
@@ -59,15 +58,4 @@ public class EnergyMarket extends AbstractAgent implements Agent {
 	}
 
 
-	//public void setDemand(double demandSet) {
-    //    this.demand = demandSet;
     }
-	
-	/*
-	 * 
-	 * Methods
-	 * Search for bids of the current time step from the graph database
-		Sort bids according to bid price
-		Determines market clearing price depending on demand.
-		Store ClearingPrice in database
-		*/
